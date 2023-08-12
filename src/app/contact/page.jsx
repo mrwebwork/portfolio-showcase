a06cc6c64a1200255d24f4ddc6762c23405747ae
mrwebwork <tech@allanswebwork.info>
2023-08-12 04:31:47 -0500

    Hide contact form component on Contact me page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -123,7 +123,7 @@ export default function Contact() {
 
       <Container className="mt-24 sm:mt-32">
         <div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
-          <ContactForm />
+          {/* <ContactForm /> */}
           <ContactDetails />
         </div>
       </Container>
